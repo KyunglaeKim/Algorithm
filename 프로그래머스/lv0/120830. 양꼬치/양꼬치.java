9e989867b8993aee3ea4452ac03ad77cefe75134
KyunglaeKim <100256825+KyunglaeKim@users.noreply.github.com>
2023-04-08 20:55:10 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        if(0 < n && n < 1000)
+        {
+         	answer = (n * 12000) + ((k - (n / 10)) * 2000);
+        }
+        return answer;
+    }
+}
